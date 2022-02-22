6237e84418381c3b4db082c9b3f9ef41a774123a
higinot <engprodhigino@gmail.com>
2022-02-21 23:51:16 -0300

    Atualizando pasta

---
+++ fundamentos/bloco-06-HTML-CSS-Forms/dia-02-Frameworks-CSS/script.js
@@ -0,0 +1,18 @@
+const checkPhotos = querySelector('[name]=checkTwo')
+
+function handleSubmit (event) {
+    event.preventDefault();
+}
+
+window.onload = function () {
+    const submitBtn = document.querySelector('#btn-send')
+    submitBtn.addEventListener('click', handleSubmit);
+
+};
+
+function sendPhotos () {
+
+}
+
+
+
