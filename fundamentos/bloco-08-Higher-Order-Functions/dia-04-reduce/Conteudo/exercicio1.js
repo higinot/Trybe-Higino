a640c25ade813e71bb396a366f8c1e0cdc9ddb25
higinot <engprodhigino@gmail.com>
2022-03-23 19:10:14 -0300

    Uppando pasta

--- fundamentos/bloco-08-Higher-Order-Functions/dia-04-reduce/Conteudo/exercicio1.js
+++ fundamentos/bloco-08-Higher-Order-Functions/dia-04-reduce/Conteudo/exercicio1.js
@@ -1,7 +1,10 @@
+//Array
 const numbers = [50, 85, -30, 3, 15];
 
+//Função getBigger ira retornar o maior valor do Array
 const getBigger = (bigger, number) => ((bigger > number) ? bigger : number);
 
+//Com reduce, o parametro bigger é o acumulado e o parametro number é o elemento.
 const bigger = numbers.reduce(getBigger, 0);
 console.log(bigger); // 85
 
