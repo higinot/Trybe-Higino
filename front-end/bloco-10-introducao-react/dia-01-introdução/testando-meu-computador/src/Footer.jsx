453c45aedb2f664671ea61582789a37c5a3c5534
higinot <engprodhigino@gmail.com>
2022-04-04 23:30:17 -0300

    Primeiro dia com React

---
+++ front-end/bloco-10-introducao-react/dia-01-introdução/testando-meu-computador/src/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+        <div className="footer">
+            <h4>Isso é so o começo</h4>
+        </div>
+        
+    );
+  }
+}
+
+export default Footer;
---
+++ front-end/bloco-10-introducao-react/dia-01-introdução/testando-meu-computador/src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+        <div className='title'>
+            <h1>Contéudos de Front-End</h1>
+        </div>
+      
+    )
+  }
+}
+
+export default Header
